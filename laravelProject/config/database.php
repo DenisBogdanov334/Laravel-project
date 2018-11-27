1910bdbb67ab9bada451a588beb96d203138a07c
Marchev,Atanas A.M <a.marchev@student.fontys.nl>
2018-11-27 13:17:33 +0100

    database now using hera

--- laravelProject/config/database.php
+++ laravelProject/config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'studmysql01.fhict.local'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'database'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', '123456'),
+            'database' => env('DB_DATABASE', 'dbi397569'),
+            'username' => env('DB_USERNAME', 'dbi397569'),
+            'password' => env('DB_PASSWORD', '397569'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
